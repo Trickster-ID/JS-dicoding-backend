21cc909e334cf5e7d466dd4fe2e30e4382a3e50e
Pikri <fakepikri@gmail.com>
2022-06-24 11:12:05 +0700

    first commit

---
+++ src/routes.js
@@ -0,0 +1,32 @@
+// const { addNoteHandler } = require("./handler");
+import { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } from "./handler.js";
+const routes = [
+  {
+    method: "POST",
+    path: "/notes",
+    handler: addNoteHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes",
+    handler: getAllNotesHandler,
+  },
+  {
+    method: "GET",
+    path: "/notes/{id}",
+    handler: getNoteByIdHandler,
+  },
+  {
+    method: "PUT",
+    path: "/notes/{id}",
+    handler: editNoteByIdHandler,
+  },
+  {
+    method: "DELETE",
+    path: "/notes/{id}",
+    handler: deleteNoteByIdHandler,
+  },
+];
+
+// module.exports = routes;
+export default routes;
